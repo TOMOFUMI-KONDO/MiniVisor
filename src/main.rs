83a881017eca0e3e8f5906c3b1ff39f845208c3a
TOMOFUMI-KONDO <ugax2kontomo0314@gmail.com>
2025-09-20 19:13:13 +0900

    Extract init_serial_port process

--- src/main.rs
+++ src/main.rs
@@ -31,37 +31,9 @@ extern "C" fn main(argc: usize, argv: *const *const u8) -> usize {
         return 4;
     };
 
-    let mut pl011 = None;
-    loop {
-        pl011 = dtb.search_node_by_compatible(b"arm,pl011", pl011.as_ref());
-
-        match &pl011 {
-            Some(d) => {
-                if !dtb.is_node_operational(d) {
-                    continue;
-                } else {
-                    break;
-                }
-            }
-
-            None => {
-                return 5;
-            }
-        }
+    if let Err(e) = init_serial_port(&dtb) {
+        return e;
     }
-
-    let pl011 = pl011.unwrap();
-    let Some((pl011_base, pl011_range)) = dtb.read_reg_property(&pl011, 0) else {
-        return 6;
-    };
-    let Ok(pl011) = drivers::pl011::Pl011::new(pl011_base, pl011_range) else {
-        return 7;
-    };
-
-    unsafe { (&raw mut PL011_DEVICE).write(MaybeUninit::new(pl011)) };
-    serial::init_default_serial_port(unsafe {
-        (&raw mut PL011_DEVICE).as_ref().unwrap().assume_init_ref()
-    });
 
     print!("Hello, world!\n");
     println!();
@@ -102,6 +74,42 @@ fn str_to_usize(s: &str) -> Option<usize> {
     }
 
     usize::from_str_radix(start?, radix).ok()
+}
+
+fn init_serial_port(dtb: &dtb::Dtb) -> Result<(), usize> {
+    let mut pl011 = None;
+    loop {
+        pl011 = dtb.search_node_by_compatible(b"arm,pl011", pl011.as_ref());
+
+        match &pl011 {
+            Some(d) => {
+                if !dtb.is_node_operational(d) {
+                    continue;
+                } else {
+                    break;
+                }
+            }
+
+            None => {
+                return Err(5);
+            }
+        }
+    }
+
+    let pl011 = pl011.unwrap();
+    let Some((pl011_base, pl011_range)) = dtb.read_reg_property(&pl011, 0) else {
+        return Err(6);
+    };
+    let Ok(pl011) = drivers::pl011::Pl011::new(pl011_base, pl011_range) else {
+        return Err(7);
+    };
+
+    unsafe { (&raw mut PL011_DEVICE).write(MaybeUninit::new(pl011)) };
+    serial::init_default_serial_port(unsafe {
+        (&raw mut PL011_DEVICE).as_ref().unwrap().assume_init_ref()
+    });
+
+    Ok(())
 }
 
 #[panic_handler]
